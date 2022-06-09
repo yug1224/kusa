e7def9da66a49c3fc18c56be9ab1deec74df5c23
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2022-06-09 20:47:17 +0900

    fix: コミットメッセージをkusaに変更

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import dayjs from "https://esm.sh/dayjs/";
+import dayjs from "https://esm.sh/dayjs@1.11.3";
 // import dayjs from "https://cdn.skypack.dev/dayjs";
 import Denomander from "https://deno.land/x/denomander@0.9.1/mod.ts";
 import * as log from "https://deno.land/std@0.121.0/log/mod.ts";
@@ -100,7 +100,7 @@ async function commit() {
       "--date",
       date.format(),
       "-m",
-      "update",
+      "kusa",
     ],
     stderr: "piped",
     stdout: "piped",
